93f1bcc1a4a4bde6f842b0b14c7307b51ca246d4
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-10-18 21:52:18 +0700

    tampilan utama dari apl, dan membuat kode jd mudah

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+// tampilan utama dari aplikasi akan disimpan,
+// sehingga membuat kode jadi lebih mudah dibaca & dikelola
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
