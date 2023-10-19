48165ee3d621531e097fa4a51426f8d67213a82a
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-10-19 11:07:28 +0700

    data dinamis

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,75 +3,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:list_kuliner/list_item.dart';
-import 'package:list_kuliner/styles.dart';
+import 'package:list_kuliner/makanan.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return ListView.builder(
+      itemCount: listMakanan.length,
       padding: EdgeInsets.all(10),
-      children: [
-        ListItem(),
-        const SizedBox(height: 20),
-        const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Icon(Icons.list_alt_sharp, size: 30),
-          SizedBox(width: 10),
-          Text('List Kuliner', style: textHeader1),
-        ]),
-        Padding(padding: EdgeInsets.only(bottom: 20)),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 8),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(255, 102, 255, 178),
-                offset: Offset(1.0, 2.0),
-                blurRadius: 6.0,
-              ),
-            ],
-          ),
-          height: 100,
-          padding: EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 10,
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 85,
-                height: 75,
-                color: Colors.grey,
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Title",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "Sub Text",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ],
+      itemBuilder: (context, index) {
+        return ListItem(
+          makanan: listMakanan[index],
+        );
+      },
     );
   }
 }
