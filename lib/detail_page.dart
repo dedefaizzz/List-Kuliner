31a7d4ec368ff6d21065d22a6da07eb4e4b69d90
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-10-19 12:09:28 +0700

    detail page

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,1 +1,42 @@
 // tampilan halaman detail kuliner
+import 'package:flutter/material.dart';
+import 'package:list_kuliner/makanan.dart';
+
+class DetailPage extends StatelessWidget {
+  final Makanan makanan;
+  const DetailPage({super.key, required this.makanan});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          makanan.gambar,
+          scale: 0.5,
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Color.fromRGBO(51, 255, 153, 1),
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
