f814f7c63e99d5a9d83e68c576563bcf573be20b
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-10-19 10:38:14 +0700

    list item

--- lib/list_item.dart
+++ lib/list_item.dart
@@ -2,12 +2,48 @@
 // turunan dari stateless widget
 
 import 'package:flutter/material.dart';
+import 'package:list_kuliner/makanan.dart';
 
 class ListItem extends StatelessWidget {
-  const ListItem({super.key});
+  final Makanan makanan;
+
+  const ListItem({super.key, required this.makanan});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // widget untuk menampilkan gambar lokal
+          Image.asset(
+            makanan.gambar,
+            height: 75,
+            width: 75,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                makanan.nama,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                makanan.deskripsi,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
